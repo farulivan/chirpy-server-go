5ace709279508588f71a3346f35cfca9348a549a
farulivan <farul@nabitu.id>
2026-05-17 21:00:04 +0700

    feat: add PostgreSQL database integration with user creation and profanity filtering 🗄️

--- validate.go
+++ validate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +11,7 @@ func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Reques
 		Body string `json:"body"`
 	}
 	type response struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	var params parameters
@@ -19,10 +20,38 @@ func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	if len(params.Body) > 140 {
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, response{Valid: true})
+	profaneWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	words := strings.Split(params.Body, " ")
+
+	for i, word := range words {
+		if _, exists := profaneWords[strings.ToLower(word)]; exists {
+			words[i] = "****"
+		}
+	}
+
+	cleaned := getCleanedBody(params.Body, profaneWords)
+
+	respondWithJSON(w, http.StatusOK, response{CleanedBody: cleaned})
+}
+
+func getCleanedBody(body string, profaneWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := profaneWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
 }
